a6449b820de9f4a865b2d538d09c4d6c3dac3c49
Filipe Cardoso <142547555+Filipcardos@users.noreply.github.com>
2026-04-18 12:28:21 -0300

    Update atividade.cpp

--- Desafios C++/Desafio1/atividade.cpp
+++ Desafios C++/Desafio1/atividade.cpp
@@ -1,2 +1,22 @@
 
-Cálculo da Média
+#Cálculo da Média#
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    float n1, n2, media;
+
+    cout << "Digite a primeira nota: ";
+    cin >> n1;
+
+    cout << "Digite a segunda nota: ";
+    cin >> n2;
+
+    media = (n1 + n2) / 2;
+
+    cout << "Media: " << media << endl;
+
+    return 0;
+}
